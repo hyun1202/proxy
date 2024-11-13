847a2ee55c389efdeb12f526e250b3bb66adbed9
hyun <apfhd0257@naver.com>
2024-11-13 16:54:44 +0900

    advice, pointcut 사용 예제

---
+++ src/test/java/hello/proxy/advisor/AdvisorTest.java
@@ -0,0 +1,99 @@
+package hello.proxy.advisor;
+
+import hello.proxy.common.advice.TimeAdvice;
+import hello.proxy.common.service.ServiceImpl;
+import hello.proxy.common.service.ServiceInterface;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.ClassFilter;
+import org.springframework.aop.MethodMatcher;
+import org.springframework.aop.Pointcut;
+import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.aop.support.NameMatchMethodPointcut;
+
+import java.lang.reflect.Method;
+
+public class AdvisorTest {
+
+    @Test
+    void advisorTest1() {
+        ServiceInterface target = new ServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        DefaultPointcutAdvisor advisor = new DefaultPointcutAdvisor(Pointcut.TRUE, new TimeAdvice());
+        proxyFactory.addAdvisor(advisor);
+        ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
+
+        proxy.save();
+        proxy.find();
+    }
+
+    @Test
+    void advisorTest2() {
+        // 직접 만든 포인트컷
+        ServiceInterface target = new ServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        DefaultPointcutAdvisor advisor = new DefaultPointcutAdvisor(new MyPointcut(), new TimeAdvice());
+        proxyFactory.addAdvisor(advisor);
+        ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
+
+        proxy.save();
+        proxy.find();
+    }
+
+    @Test
+    void advisorTest3() {
+        // 스프링이 제공하는 포인트컷
+        ServiceInterface target = new ServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        NameMatchMethodPointcut pointcut = new NameMatchMethodPointcut();
+        pointcut.setMappedNames("save");
+        DefaultPointcutAdvisor advisor = new DefaultPointcutAdvisor(pointcut, new TimeAdvice());
+        proxyFactory.addAdvisor(advisor);
+        ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
+
+        proxy.save();
+        proxy.find();
+    }
+
+    static class MyPointcut implements Pointcut {
+
+        @Override
+        public ClassFilter getClassFilter() {
+            return ClassFilter.TRUE;
+        }
+
+        @Override
+        public MethodMatcher getMethodMatcher() {
+            return new MyMethodMatcher();
+        }
+    }
+
+    @Slf4j
+    static class MyMethodMatcher implements MethodMatcher {
+
+        private String matchName = "save";
+
+        // false일 떄 해당 메소드 실행.
+        @Override
+        public boolean matches(Method method, Class<?> targetClass) {
+            boolean result = method.getName().equals(matchName);
+            log.info("포인트컷 호출 method={} targetClass={}", method.getName(), targetClass);
+            log.info("포인트컷 결과 result={}", result);
+            return result;
+        }
+
+        // false: 정적 정보만 사용하므로 내부에서 캐싱을 통한 성능 향상 가능
+        // true: 매개변수가 동적으로 변경된다고 가정하므로 캐싱을 하지 않음
+        @Override
+        public boolean isRuntime() {
+            return false;
+        }
+
+        // true일 때 해당 메소드 실행
+        @Override
+        public boolean matches(Method method, Class<?> targetClass, Object... args) {
+            return false;
+        }
+    }
+}
